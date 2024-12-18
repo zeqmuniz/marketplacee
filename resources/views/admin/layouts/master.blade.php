ae4b4dbe3fef320d9bf0812d144d802882cc75e8
Ezequiel Muniz <muniz2405@gmail.com>
2024-12-18 13:04:40 -0300

    commit 10

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Painel Administrativo  &mdash; StartEscola.com</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/summernote/summernote-bs4.css') }}">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+        <!-- START NAVBAR -->
+            @include('admin.layouts.navbar')
+        <!-- END NAVBAR-->
+
+        <!-- START SIDEBAR - Ezequiel Muniz-->
+            @include('admin.layouts.sidebar')
+        <!-- END SIDEBAR-->
+
+      <!-- START MAIN CONTENT - Ezequiel Muniz-->
+      <div class="main-content">
+
+        @yield('content')
+
+      </div>
+      <!-- END MAIN CONTENT-->
+
+      <!-- START FOOTER - Ezequiel Muniz-->
+      <footer class="main-footer">
+        <div class="footer-left">
+          Todos os Direitos Reservados &copy; <?= date('Y')?> <div class="bullet"></div> Desenvolvido por <a href="https://www.startescola.com/">Start Escola de Treinamento Ministerial</a> versão 1.0
+        </div>
+        <div class="footer-right">
+
+        </div>
+      </footer>
+      <!-- END FOOTER-->
+
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('backend/assets/modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/popper.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/tooltip.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/moment.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/stisla.js') }}"></script>
+
+  <!-- JS Libraies -->
+  <script src="{{ asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/chart.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/summernote/summernote-bs4.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="{{ asset('backend/assets/js/page/index-0.js') }}"></script>
+
+  <!-- Template JS File -->
+  <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
+</body>
+</html>
